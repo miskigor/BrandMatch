6cf47400604f513e0d1d256d93163adc74386ec2
miskigor <royalexpert1@gmail.com>
2026-01-04 21:26:51 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -473,7 +473,7 @@ Giveaway / aktivacije zajednice`,
                   Reiseziel Kroatien
                 </h3>
                 <p className="text-sm opacity-70 font-light leading-relaxed mb-4">
-                  Kuće za odmor i apartmani u Bilicama
+                  Kuće za odmor i apartmani u Bilicama                                                 
                 </p>
                 <a 
                   href="https://reiseziel-kroatien.de/" 
